12c06f99917cd5cc8a97439e394d7e0bc6a45002
fabio98p <parisifabio98@gmail.com>
2021-03-09 16:39:32 +0100

    campominato v1.0 finito??

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,27 +16,46 @@ console.log("ciao ale");
 // 	default:
 // 		break;
 // }
-var bomb = []
-var x = 0
+var bomb = [];
+var x = 0;
 for (let i = 0; i < 16; i++) {
 	do {
-		x = random(1, 100)
+		x = random(1, 100);
 	} while (bomb.includes(x));
-	bomb.push(x)
+	bomb.push(x);
 }
 
 var arrayWithBomb = []
 
 for (let i = 1; i <= 100; i++) {
+	//arrayWithBomb.push(bomb.includes(i));
+
 	if (bomb.includes(i)) {
-		arrayWithBomb.push("bomb")
+		arrayWithBomb.push(false);
 	} else {
-		arrayWithBomb.push(i)
+		arrayWithBomb.push(true);
 	}
 }
 
+var arrayPlayer = []
+var y
 
-console.log(bomb, arrayWithBomb);
+for (let i = 1; i <= (100 - 16); i++) {
+
+	do {
+		y = parseInt(prompt("scrivi un numero da 1 a 100", "1"));
+	} while (arrayPlayer.includes(y));
+
+	if (!arrayWithBomb[y - 1]) {//-1 perche è un array
+		console.log("game over");
+		console.log("hai fatto " + arrayPlayer.length + " punti");
+		break;
+	}
+	arrayPlayer.push(y)
+
+}
+
+
 
 function random(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
